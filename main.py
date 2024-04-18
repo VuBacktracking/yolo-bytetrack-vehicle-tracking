50970fddc0b7562a0c75655b0747f27e0325a48e
VuBacktracking <vubacktracking@users.noreply.github.com>
2024-04-18 21:59:32 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from trackyolo import ObjectTracking
+from ultralytics import YOLO
+
+INPUT_PATH = "assets/video/vehicle-counting.mp4"
+OUTPUT_PATH = "assets/video/vehicle-counting-result.mp4"
+
+if __name__ == "__main__":
+    obj = ObjectTracking(INPUT_PATH, OUTPUT_PATH)
+    obj.process()
