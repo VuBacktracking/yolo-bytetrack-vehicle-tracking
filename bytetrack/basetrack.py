38c45c2f0ef940b2511ddf1132839e19a55b6c99
VuBacktracking <vubacktracking@users.noreply.github.com>
2024-04-18 11:16:25 +0700

    Modified bytetrack

--- bytetrack/basetrack.py
+++ bytetrack/basetrack.py
@@ -1,40 +1,51 @@
-import numpy as np
 from collections import OrderedDict
+from enum import Enum
+
+import numpy as np
 
 
-class TrackState(object):
+class TrackState(Enum):
     New = 0
     Tracked = 1
     Lost = 2
     Removed = 3
 
 
-class BaseTrack(object):
+class BaseTrack:
     _count = 0
 
-    track_id = 0
-    is_activated = False
-    state = TrackState.New
+    def __init__(self):
+        self.track_id = 0
+        self.is_activated = False
+        self.state = TrackState.New
 
-    history = OrderedDict()
-    features = []
-    curr_feature = None
-    score = 0
-    start_frame = 0
-    frame_id = 0
-    time_since_update = 0
+        self.history = OrderedDict()
+        self.features = []
+        self.curr_feature = None
+        self.score = 0
+        self.start_frame = 0
+        self.frame_id = 0
+        self.time_since_update = 0
 
-    # multi-camera
-    location = (np.inf, np.inf)
+        # multi-camera
+        self.location = (np.inf, np.inf)
 
     @property
-    def end_frame(self):
+    def end_frame(self) -> int:
         return self.frame_id
 
     @staticmethod
-    def next_id():
+    def next_id() -> int:
         BaseTrack._count += 1
         return BaseTrack._count
+
+    @staticmethod
+    def reset_counter():
+        BaseTrack._count = 0
+        BaseTrack.track_id = 0
+        BaseTrack.start_frame = 0
+        BaseTrack.frame_id = 0
+        BaseTrack.time_since_update = 0
 
     def activate(self, *args):
         raise NotImplementedError
